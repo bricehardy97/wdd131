25905b608c381c42b247023205114e8fdcec9952
bricehardy97 <har24047@byui.edu>
2025-04-05 17:41:22 -0600

    updates

--- week03/scripts_places/places.js
+++ week03/scripts_places/places.js
@@ -15,10 +15,35 @@ function setLastModified() {
     lastModified.innerHTML = "Date last modified: " + document.lastModified;
 }
 
+function calculateWindChill(windSpeed, temperature){
+    console.log(windSpeed,temperature)
+    const windChill = 
+                    35.74+
+                    (0.6215*temperature)-
+                    (35.75*Math.pow(windSpeed,0.16))+
+                    (0.4275*temperature* Math.pow(windSpeed,0.16))
+
+    return windChill
+}
+
+function insertWindChill(windChillDecimal){
+    const windChillElement = document.getElementById("windChill")
+    windChillElement.innerHTML = windChillDecimal
+}
+
 function setup(){
     
     setYear();
     setLastModified();
+    const tempF = 15;
+    const windSpedMPH = 0.5;
+    const isCelcius = true;
+
+    if (??? && ??? && ){
+        const calculatedWindChill = calculateWindChill(windSpedMPH, tempF)
+        insertWindChill(calculatedWindChill);
+    }
 }
 
+// ENTRY POINT
 setup();
